a1754ff24ceca0fa3e47745b566729bad4ea2a90
thurein <thureintun@acedatasystems.com>
2024-07-16 16:55:10 +0630

    added search server action function

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,16 @@
 import Link from "next/link";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { redirect } from "next/navigation";
 
 
 export default async function header() {
+
+    async function search(formData) {
+        "use server";
+        const q = formData.get("q");
+        redirect(`/search?q=${q}`);
+    }
 
     return (
         <nav className="flex justify-between items-center border
@@ -9,6 +18,15 @@ rounded border-slate-300 px-4 py-2 my-4">
             <h1 className="font-bold text-lg">
                 <Link href="/">Next Movies</Link>
             </h1>
+
+            <form action={search} className="flex gap-2">
+                <Input
+                    type="text"
+                    name="q"
+                    placeholder="Search Movies"
+                />
+                <Button type="submit">Search</Button>
+            </form>
         </nav>
     )
 }
